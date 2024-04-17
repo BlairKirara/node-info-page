1d1fc806d962f790a1f35030c33b107dbc0ffcd3
BlairKirara <bluberry.muffin.15@gmail.com>
2024-04-17 16:30:21 +0200

    Add content of the folder to the repo

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var { URL } = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    var q = new URL(req.url, "http://localhost:8080");
+    var filename = "." + q.pathname + ".html";
+    if (q.pathname === "/"){
+        filename = "./index.html";
+    } else if (q.pathname != "/about" & q.pathname != "/contact-me"){
+        filename = "./404.html";
+    }
+    fs.readFile(filename, function(err, data){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
